6fa99369504a5dab07180015ddaa342a3e0b5ac2
RonV42 <34961476+RonV42@users.noreply.github.com>
2022-06-16 11:42:10 -0500

    Updated tosay function to include null volume options

--- LANnouncer/LANnouncer Alerter.groovy
+++ LANnouncer/LANnouncer Alerter.groovy
@@ -1,16 +1,11 @@
 /**
- *  LANnouncer Alerter (Formerly LANdroid - but Google didn't like that much.)
+ *  LANnouncer Alerter-Hubitat (Originally LANdroid Alerter)
  *
- *  Requires the LANnouncer android app; https://play.google.com/store/apps/details?id=com.keybounce.lannouncer
- *  See http://www.keybounce.com/LANdroidHowTo/LANdroid.html for full downloads and instructions.
- *  SmartThings thread: https://community.smartthings.com/t/android-as-a-speech-alarm-device-released/30282/12
- *  
- * Note: Only Siren and Strobe from the U.I. or Alarm capabilities default to continuous.
+ *  Requires the Android TTSService (aka LANnouncer  in the Play Store; https://play.google.com/store/apps/details?id=com.keybounce.lannouncer )
  *
- *  Version 1.25 22 July 2016
- * 
- *
- *  Copyright 2015-2016 Tony McNamara
+ *  Copyright 2015 Tony McNamara
+ *  8/1/2019 - Further modifyed by Ron Vargo to remove SmartThings references and support spaces in TTS strings
+ *  6/10/2022 - Added the volume valute to anncouncemnt function to be null due to changes in Hubitat for TTS to included
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -21,39 +16,24 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- * To Do: Add string return to Image Capture attribute
- *
  */
 
 metadata {
-    definition (name: "LANnouncer Alerter", namespace: "KeyBounce", author: "Tony McNamara") {
+    definition (name: "LANnouncer Alerter-Hubitat", namespace: "RonV42", author: "Ron Vargo") {
         capability "Alarm"
         capability "Speech Synthesis"
         capability "Notification"
         capability "Tone"
-        capability "Image Capture"
-        attribute  "LANdroidSMS","string"
-        /* Per http://docs.smartthings.com/en/latest/device-type-developers-guide/overview.html#actuator-and-sensor */
-        capability "Sensor"
-        capability "Actuator"
-
-        // Custom Commands
-        /** Retrieve image, formatted for SmartThings, from camera by name. */
-        command "chime"
-        command "doorbell"
-        command "ipCamSequence", ["number"]
-        command "retrieveAndWait", ["string"]
-        command "retrieveFirstAndWait"
-        command "retrieveSecondAndWait"
+        attribute  "LANnouncerSMS","string"
     }
     preferences {
         input("DeviceLocalLan", "string", title:"Android IP Address", description:"Please enter your tablet's I.P. address", defaultValue:"" , required: false, displayDuringSetup: true)
         input("DevicePort", "string", title:"Android Port", description:"Port the Android device listens on", defaultValue:"1035", required: false, displayDuringSetup: true)
         input("ReplyOnEmpty", "bool", title:"Say Nothing", description:"When no speech is found, announce LANdroid?  (Needed for the speech and notify tiles to work)", defaultValue: true, displayDuringSetup: true)
-        input("AlarmContinuous", "bool", title:"Continuous Alarm (vs 10 sec.)", description: "When on, the alarm will sound until Stop is issued.", defaultValue: false, displayDuringSetup: true)
     }
 
     simulator {
+        
     }
 
     tiles {
@@ -63,28 +43,15 @@ metadata {
             state "siren", label:'siren!', action:'alarm.off', icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13"
             state "both", label:'alarm!', action:'alarm.off', icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13"
         }
+
         standardTile("strobe", "device.alarm", inactiveLabel: false, decoration: "flat") {
             state "default", label:'', action:"alarm.strobe", icon:"st.secondary.strobe"
         }
-        
         standardTile("siren", "device.alarm", inactiveLabel: false, decoration: "flat") {
             state "default", label:'', action:"alarm.siren", icon:"st.secondary.siren"
-        }
-        standardTile("off", "device.alarm", inactiveLabel: false, decoration: "flat") {
-            state "default", label:'Off', action:"alarm.off", icon:"st.quirky.spotter.quirky-spotter-sound-off"
         }       
-        
-        /* Apparently can't show image attributes on tiles. */
-        standardTile("take", "device.image", width: 1, height: 1, canChangeIcon: false, canChangeBackground: false) 
-        {
-            state "take", label: "Take", action: "Image Capture.take", icon: "st.camera.camera", backgroundColor: "#FFFFFF", nextState:"taking"
-            state "taking", label:'Taking', action: "", icon: "st.camera.take-photo", backgroundColor: "#53a7c0"
-            state "image", label: "Take", action: "Image Capture.take", icon: "st.camera.camera", backgroundColor: "#FFFFFF", nextState:"taking"
-        }
 
-        
-        standardTile("speak", "device.speech", inactiveLabel: false, decoration: "flat") 
-        {
+        standardTile("speak", "device.speech", inactiveLabel: false, decoration: "flat") {
             state "default", label:'Speak', action:"Speech Synthesis.speak", icon:"st.Electronics.electronics13"
         }
         standardTile("toast", "device.notification", inactiveLabel: false, decoration: "flat") {
@@ -92,80 +59,62 @@ metadata {
         }
         standardTile("beep", "device.tone", inactiveLabel: false, decoration: "flat") {
             state "default", label:'Tone', action:"tone.beep", icon:"st.Entertainment.entertainment2"
-        }
-        carouselTile("cameraDetails", "device.image", width: 3, height: 2) { }
+        }       
 
-        main (["alarm", "take"]);
-        details(["alarm","strobe","siren","off","speak", "take","toast","beep", "cameraDetails"]);
+        main "alarm"
+        details(["alarm","strobe","siren","speak","toast","beep"])
     }
 }
 
-/** Generally matches TTSServer/app/build.gradle */
-String getVersion() {return "24 built July 2016";}
+// parse events into attributes
+def parse(String description) {
+    log.debug "Parsing '${description}'"
+    // TODO: handle 'alarm' attribute
 
+}
 
-/** Alarm Capability, Off.
- *  Turns off both the strobe and the alarm.
- *  Necessary when in continuous mode. 
- */
+// handle commands
 def off() {
     log.debug "Executing 'off'"
-    sendEvent(name:"alarm", value:"off")
-    def command="&ALARM=STOP&FLASH=STOP&"+getDoneString();
-    return sendCommands(command)
+    // TODO: handle 'off' command
 }
 
-/** Alarm Capability: Strobe
- *  Flashes the camera light, if any.
- */
 def strobe() {
     log.debug "Executing 'strobe'"
-    // For illustration, switch to siren and sendEvent after.
-    def command= (AlarmContinuous?"&FLASH=CONTINUOUS&":"&FLASH=STROBE&")+getDoneString();
-    // def command=(AlarmContinuous?"&ALARM=SIREN:CONTINUOUS&":"&ALARM=SIREN&")+getDoneString();
-    def hubAction = sendCommands(command)
-    sendEvent(name:"alarm", value:"strobe")
-    return hubAction;
+    // TODO: handle 'strobe' command
+    def command="&FLASH=STROBE&"+getDoneString()
+    sendCommands(command)
+
 }
 
-/** Alarm Capability: Siren 
- *  Sounds the siren, either continuous or for a brief period, depending on setting.
- *  If continuous, Stop should be called later.
- */
 def siren() {
     log.debug "Executing 'siren'"
-    sendEvent(name:"alarm", value:"siren")
-    def command=(AlarmContinuous?"&ALARM=SIREN:CONTINUOUS&":"&ALARM=SIREN&")+getDoneString();
-    return sendCommands(command);
+    // TODO: handle 'siren' command
+    def command="&ALARM=SIREN&"+getDoneString()
+    sendCommands(command)
 }
 
-/** Tone Capability: Beep
- *  Sounds a short beep
- */
 def beep() {
     log.debug "Executing 'beep'"
+    // TODO: handle 'siren' command
     def command="&ALARM=CHIME&"+getDoneString()
-    return sendCommands(command);
+    sendCommands(command)
 }
 
 def both() {
     log.debug "Executing 'both'"
-    sendEvent(name:"alarm", value:"both")
+    // TODO: handle 'both' command
     def command="&ALARM=ON&FLASH=ON&"+getDoneString()
-    if (AlarmContinuous)
-    {
-        command="&ALARM=SIREN:CONTINUOUS&FLASH=CONTINUOUS&"+getDoneString()
-    }
-    return sendCommands(command);
+    sendCommands(command)
 }
 
-/** speechSynthesis Capability: Speak
- */
-def speak(toSay) {
+// Added volume = null based on changes by brevel and that we don't support volume calls
+
+def speak(toSay, volume = null) {
     log.debug "Executing 'speak'"
     if (!toSay?.trim()) {
         if (ReplyOnEmpty) {
-            toSay = "LANnouncer Version ${version}"
+            toSay = "Landroid Speech Synthesizer"
         }
     }
     
@@ -173,104 +122,54 @@ def speak(toSay) {
 
     if (toSay?.trim()) {
         def command="&SPEAK="+toSay+"&"+getDoneString()
-        return sendCommands(command)
+        sendCommands(command)
     }
 }
 
-/** Notification capability: deviceNotification
- */
 def deviceNotification(toToast) {
     log.debug "Executing notification with "+toToast
     if (!toToast?.trim()) {
         if (ReplyOnEmpty) {
-            toToast = "LANnouncer Version ${version}";
+            toToast = "Landroid Speech Synthesizer"
         }
     }
+
+     toToast = toToast.replaceAll("\\s","%20")   
+    
     if (toToast?.trim()) {
         def command="&TOAST="+toToast+"&"+getDoneString()
-        return sendCommands(command)
+        sendCommands(command)
     }
 }    
 
-def chime() {
-    log.debug "Executing 'chime'"
-    // TODO: handle 'siren' command
-    def command="&ALARM=CHIME&"+getDoneString()
-    return sendCommands(command)
-}
-
-def doorbell() {
-    log.debug "Executing 'doorbell'"
-    // TODO: handle 'siren' command
-    def command="&ALARM=DOORBELL&"+getDoneString()
-    return sendCommands(command)
-}
-
-def ipCamSequence(cameraNumber) {
-    def camera = (cameraNumber==1?"FIRST":"SECOND");
-    def command="&RETRIEVESEQ="+cameraNumber+"&"+getDoneString()
-    return sendIPCommand(command, true)
-}
-
-
-def retrieveFirstAndWait() {
-    retrieveAndWait("FIRST");
-}
-def retrieveSecondAndWait() {
-    retrieveAndWait("SECOND");
-}
-def retrieveAndWait(cameraName) {
-    log.info("Requesting image from camera ${cameraName}");
-    def command="&RETRIEVE="+cameraName+"&STSHRINK=TRUE&"+getDoneString()
-    return sendIPCommand(command, true)
-}
-
-
-def take() {
-    // This won't result in received file. Can't handle large or binaries in hub.
-    log.debug "Executing 'take'"
-    def command="&PHOTO=BACK&STSHRINK=TRUE&"+getDoneString()
-    return sendIPCommand(command, true)
-}
-
-/** Send to IP and to SMS as appropriate 
- *  The caller MUST return the value, which is the hubAction.
- *  As of version 1.25, does not "send" the command so much as 
- *  request that the calling service send it.
- */
+/* Send to IP and to SMS as appropriate */
 private sendCommands(command) {
-    log.info "Command request: "+command
-    sendSMSCommand(command)
-    return sendIPCommand(command)
+    log.debug "Command request: "+command
+//    sendSMSCommand(command)
+    sendIPCommand(command)
 }
 
-/** Prepares the hubAction to be executed.
- *  Pre-V25, this was executed in-line.  
- *  Now it is returned, not executed, and must be returned up the calling chain.
- */
-private sendIPCommand(commandString, sendToS3 = false) {
-    log.info "Sending command "+ commandString+" to "+DeviceLocalLan+":"+DevicePort
+private sendIPCommand(commandString ) {
+    log.debug "Sending command to "+DeviceLocalLan
+    sendEvent(name: "LANnouncerIP", value: commandString, isStateChange: true)
     if (DeviceLocalLan?.trim()) {
-        def hosthex = convertIPtoHex(DeviceLocalLan)
+        def host = DeviceLocalLan 
+        def hosthex = convertIPtoHex(host)
         def porthex = convertPortToHex(DevicePort)
-        device.deviceNetworkId = "$hosthex:$porthex"
+        device.deviceNetworkId = "$hosthex:$porthex" 
 
         def headers = [:] 
-        headers.put("HOST", "$DeviceLocalLan:$DevicePort")
+        headers.put("HOST", "$host:$DevicePort")
 
         def method = "GET"
 
-        def hubAction = new hubitat.device.HubAction(
+        def hubAction = new hubitat.device.HubAction([
             method: method,
             path: "/"+commandString,
-            headers: headers
-            );
-        if (sendToS3 == true)
-        {
-            hubAction.options = [outputMsgToS3:true];
-        }
-        log.debug hubAction
-        return hubAction;
+            headers: headers],
+            device.deviceNetworkId
+            )
+        hubAction
     }
 }
 
@@ -278,7 +177,7 @@ private sendSMSCommand(commandString) {
     def preface = "+@TTSSMS@+"
     def smsValue = preface+"&"+commandString
     state.lastsmscommand = smsValue
-    sendEvent(name: "LANdroidSMS", value: smsValue, isStateChange: true)
+    sendEvent(name: "LANnouncerSMS", value: smsValue, isStateChange: true)
     /*
     if (SMSPhone?.trim()) {
         sendSmsMessage(SMSPhone, preface+"&"+commandString)
@@ -290,69 +189,16 @@ private String getDoneString() {
     return "@DONE@"
 }
 
-def parse(String description) {
-    log.debug "Parsing '${description}'"
-    def map = parseLanMessage(description);
-    log.debug "As LAN: " + map;
-    if ((map.headers) && (map.headers.'Content-Type' != null) && (map.headers.'Content-Type'.contains("image/jpeg")) )
-    {   //  Store the file
-      if(map.body) 
-      {
-            storeImage(getPictureName(), map.body);
-      }
-    }
-/* 'index:0F, mac:0073E023A13A, ip:C0A80114, port:040B, requestId:f9036fb2-9637-40b8-b2c5-71ba5a09fd3e, bucket:smartthings-device-conn-temp, key:fc8e3dfd-5035-40a2-8adc-a312926f9034' */
-
-    else if (map.bucket && map.key)
-    { //    S3 pointer; retrieve image from it to store.
-        try {
-            def s3ObjectContent; // Needed for scope of try-catch
-            def imageBytes = getS3Object(map.bucket, map.key + ".jpg")
-
-            if(imageBytes)
-            {
-                log.info ("Got image bytes; saving them.")
-                s3ObjectContent = imageBytes.getObjectContent()
-                def bytes = new ByteArrayInputStream(s3ObjectContent.bytes)
-                storeImage(getPictureName(), bytes)
-            }
-        }
-        catch(Exception e) 
-        {
-            log.error e
-        }
-        finally {
-            //explicitly close the stream
-            if (s3ObjectContent) { s3ObjectContent.close() }
-        }        
-    }
-}
-
-
-// Image Capture handling
-/* Note that images are stored in https://graph.api.smartthings.com/api/s3/smartthings-smartsense-camera/[IMAGE-ID4], 
- * where [IMAGE-ID] is listed in the IDE under My Devices > [Camera] > Current States: Image. That page is updated as pictures are taken.
- */
-
-
-private getPictureName() {
-    def pictureUuid = java.util.UUID.randomUUID().toString().replaceAll('-', '')
-    log.debug pictureUuid
-    def picName = device.deviceNetworkId.replaceAll(':', '') + "_$pictureUuid" + ".jpg"
-    return picName
-}
-
 
 private String convertIPtoHex(ipAddress) { 
-    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02X', it.toInteger() ) }.join()
+    String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
     log.debug "IP address entered is $ipAddress and the converted hex code is $hex"
     return hex
 
 }
 
 private String convertPortToHex(port) {
-    String hexport = port.toString().format( '%04X', port.toInteger() )
+    String hexport = port.toString().format( '%04x', port.toInteger() )
     log.debug hexport
     return hexport
 }
-
